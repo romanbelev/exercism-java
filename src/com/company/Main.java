31970ab9e27d4959dbf20ed0036e7a8c426ce080
Roman <ram219219@gmail.com>
2020-09-13 21:33:28 +0300

    Add

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -13,6 +13,28 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        NumberCreater numberCreater = new NumberCreater();
+        String phoneNumber = numberCreater.create(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 0});
+
+        System.out.println(phoneNumber);
+    }
+}
+
+class NumberCreater {
+    public String create(int[] numbers) {
+        StringBuilder phoneNumber = new StringBuilder();
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (i == 0) {
+                phoneNumber.append("(");
+            } else if (i == 3) {
+                phoneNumber.append(") ");
+            } else if (i == 6) {
+                phoneNumber.append("-");
+            }
+            phoneNumber.append(numbers[i]);
+        }
+
+        return phoneNumber.toString();
     }
 }
