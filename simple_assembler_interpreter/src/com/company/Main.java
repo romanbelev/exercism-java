c7e0b880cb3a6928c0275b4963936886bedd10a3
Roman <ram219219@gmail.com>
2020-09-19 21:47:46 +0200

    Add last test fix

--- simple_assembler_interpreter/src/com/company/Main.java
+++ simple_assembler_interpreter/src/com/company/Main.java
@@ -10,7 +10,7 @@ public class Main {
     public static void main(String[] args) {
 
         //String[] program = {"mov a 5", "inc a", "dec a", "dec a", "jnz a -1", "inc a"};
-        String[] program = {"mov a -10", "mov b a", "inc a", "dec b", "jnz a -2"};
+        String[] program = {"mov c 12","mov b 0","mov a 200","dec a","inc b","jnz a -2","dec c","mov a b","jnz c -5","jnz 0 1","mov c a"};
         SimpleAssemblerInterpret sai = new SimpleAssemblerInterpret(program);
         sai.interpret();
     }
@@ -19,89 +19,106 @@ public class Main {
 class SimpleAssemblerInterpret {
     String[] program;
     Map<String, Integer> variable = new HashMap<String, Integer>();
+    Map<String, Integer> tempVariable = new HashMap<String, Integer>();
 
     SimpleAssemblerInterpret(String... program) {
         this.program = program;
     }
 
     public void interpret() {
-        parseVariable();
         runInstruction();
         System.out.println(variable);
     }
 
-    public void parseVariable() {
 
-        for (int i = 0; i < program.length; i++) {
-            String commandLine = program[i];
+    public void movParseInstruction(String commandLine) {
+        int variableValue;
+        String variableName;
 
-            int variableValue;
-            String variableName;
+        Pattern variableNamePattern = Pattern.compile("mov\\s(\\w*)\\s(-?[a-z0-9]*)");
+        Matcher matcher = variableNamePattern.matcher(commandLine);
 
-            Pattern variableNamePattern = Pattern.compile("mov\\s(\\w*)\\s(-?[a-z0-9]*)");
-            Matcher matcher = variableNamePattern.matcher(commandLine);
+        if (matcher.find()) {
+            try {
+                variableValue = Integer.parseInt(matcher.group(2).trim());
+            } catch (NumberFormatException e) {
+                String variableSecondName = matcher.group(2).trim();
+                variableValue = variable.get(variableSecondName);
+            }
 
-            if (matcher.find()) {
-                // System.out.println(matcher.group(0));
-                // System.out.println(matcher.group(1));
-                // System.out.println(matcher.group(2));
+            variableName = matcher.group(1).trim();
+            variable.put(variableName, variableValue);
+        }
+    }
 
-                try {
-                    variableValue = Integer.parseInt(matcher.group(2).trim());
-                } catch(NumberFormatException e) {
-                    String variableSecondName = matcher.group(2).trim();
-                    variableValue = variable.get(variableSecondName);
-                }
+    public void incParseInstruction(String commandLine) {
+        Pattern incPattern = Pattern.compile("inc\\s([a-z]*)");
+        Matcher incMatcher = incPattern.matcher(commandLine);
 
-                variableName = matcher.group(1).trim();
-                variable.put(variableName, variableValue);
+        if (incMatcher.find()) {
+            try {
+                String variableName = incMatcher.group(1).trim();
+                int value = variable.get(variableName);
+                variable.put(variableName, value + 1);
+            } catch (NumberFormatException e) {
+                System.out.println("Format error in increment instruction");
             }
         }
+    }
 
+    public void decParseInstruction(String commandLine) {
+        Pattern decPattern = Pattern.compile("dec\\s([a-z]*)");
+        Matcher decMatcher = decPattern.matcher(commandLine);
+
+        if (decMatcher.find()) {
+            try {
+                String variableName = decMatcher.group(1).trim();
+                int value = variable.get(variableName);
+                variable.put(variableName, value - 1);
+            } catch (NumberFormatException e) {
+                System.out.println("Format error in decrement instruction");
+            }
+        }
     }
 
     public void runInstruction() {
-        // String code = "inc 20";
         for (int i = 0; i < program.length; i++) {
             String commandLine = program[i];
+            // String commandLine = "mov a 100";
 
-            Pattern incPattern = Pattern.compile("inc\\s([a-z]*)");
-            Pattern decPattern = Pattern.compile("dec\\s([a-z]*)");
-            Pattern jnzPattern = Pattern.compile("jnz\\s([a-z]?)\\s(-?[0-9]*)");
-            Matcher incMatcher = incPattern.matcher(commandLine);
-            Matcher decMatcher = decPattern.matcher(commandLine);
-            // String command = "jnz a -1";
+            movParseInstruction(commandLine);
+            incParseInstruction(commandLine);
+            decParseInstruction(commandLine);
+
+            Pattern jnzPattern = Pattern.compile("jnz\\s([0-9a-z]?)\\s(-?[0-9a-z]*)");
             Matcher jnzMatcher = jnzPattern.matcher(commandLine);
 
-            // Increment
-            if (incMatcher.find()) {
-                try {
-                    String variableName = incMatcher.group(1).trim();
-                    int value = variable.get(variableName);
-                    variable.put(variableName, value + 1);
-                } catch (NumberFormatException e) {
-                    System.out.println("Format error in increment instruction");
-                }
-            }
-
-            // Decrement
-            if (decMatcher.find()) {
-                try {
-                    String variableName = decMatcher.group(1).trim();
-                    int value = variable.get(variableName);
-                    variable.put(variableName, value - 1);
-                } catch (NumberFormatException e) {
-                    System.out.println("Format error in decrement instruction");
-                }
-            }
-
-            // Jnz jnz a -1
             if (jnzMatcher.find()) {
-                String variableName = jnzMatcher.group(1).trim();
-                int variableValue = variable.get(variableName);
-                int steps = Integer.parseInt(jnzMatcher.group(2));
-                // System.out.println(variableValue);
+                int variableValue = 0;
+                int variableTempValue = 0;
 
+                try {
+                    variableTempValue = Integer.parseInt(jnzMatcher.group(1).trim());
+                    if (tempVariable.containsKey("generated_name_" + i)) {
+                        variableTempValue = tempVariable.get("generated_name_" + i);
+                        if (variableTempValue > 0) {
+                            tempVariable.put("generated_name_" + i, variableTempValue - 1);
+                        }
+                    } else {
+                        if (variableTempValue > 0) {
+                            tempVariable.put("generated_name_" + i, variableTempValue - 1);
+                        } else {
+                            tempVariable.put("generated_name_" + i, variableTempValue);
+                        }
+                    }
+
+                    variableValue = variableTempValue;
+                } catch (NumberFormatException e) {
+                    String variableName = jnzMatcher.group(1).trim();
+                    variableValue = variable.get(variableName);
+                }
+
+                int steps = Integer.parseInt(jnzMatcher.group(2));
                 if (variableValue == 0) {
                     System.out.println("jnz goes 0 to the next instruction --->");
                 } else if (steps < 0) {
@@ -111,9 +128,9 @@ class SimpleAssemblerInterpret {
                     System.out.println("jnz goes forward --->");
                     i = i + (Math.abs(steps) - 1);
                 }
-
-                // System.out.println(jnzMatcher.group(2));
             }
+
+
 
 
 
