95cfe7ee08c118cf420d7da052141cb5a01f641a
Roman <ram219219@gmail.com>
2020-10-26 20:55:14 +0200

    Add

---
+++ javatest_second/src/com/company/Main.java
@@ -0,0 +1,48 @@
+/*
+Roman Bieliev 1K211
+Lab 1
+ */
+
+package com.company;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        boolean isRepeat = false;
+        Scanner scanner = new Scanner(System.in);
+
+        do {
+            // Insert number
+            System.out.print("Insert number to find divisor: ");
+            int num = scanner.nextInt();
+            Set<Integer> numbers = findDivisor.find(num);
+
+            // Results
+            for (int number : numbers) {
+                System.out.println(number);
+            }
+
+            // Repeat
+            System.out.print("Do you wont to find more (1 - Yes / 0 - No) ?: ");
+            isRepeat = (scanner.nextInt() == 1);
+
+        } while (isRepeat);
+    }
+}
+
+class findDivisor {
+    public static Set<Integer> find(int number) {
+        int div = number;
+        Set<Integer> numbers = new TreeSet<Integer>();
+
+        while (div > 0) {
+            if (number % div == 0) {
+                numbers.add(div);
+            }
+            div--;
+        }
+
+        return numbers;
+    }
+}
