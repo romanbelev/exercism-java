3e2d29a6f3e5ef29c3a87bedb552fd96c3ffed34
Roman <ram219219@gmail.com>
2020-11-14 23:58:50 +0200

    Add all

---
+++ engine_maven/src/main/java/engine/Main.java
@@ -0,0 +1,25 @@
+package engine;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        Parent mainRoot = FXMLLoader.load(getClass().getResource("../../resources/sample.fxml"));
+
+        Scene scene = new Scene(mainRoot);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("First Application");
+        primaryStage.show();
+    }
+}
---
+++ engine_maven/src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module engine {
+    requires javafx.controls;
+    requires javafx.graphics;
+    requires javafx.fxml;
+
+    opens engine;
+}
